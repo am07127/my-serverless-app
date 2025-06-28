6fbfb84f6f361de6e3a96e4b2e3e634d7bd855e9
am07127 <am07127@st.habib.edu.pk>
2025-06-28 18:53:28 +0500

    this is a blog page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,6 +40,7 @@ export default function Home() {
             {message ? message : "Loading message..."}
           </li>
           <p><Link href="/about">About Us</Link></p> 
+          <p><Link href="/blog">Blog</Link></p>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
